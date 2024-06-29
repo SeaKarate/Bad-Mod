a81eb7fd134d3fd73516d26d755b84530c1bd0b4
SeaKarate <creeperhunteralt@gmail.com>
2024-06-29 15:08:35 +0530

    Initial commit

---
+++ src/main/java/com/kjchillin/template/TemplateClient.java
@@ -0,0 +1,10 @@
+package com.kjchillin.template;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TemplateClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+        
+    }
+}
